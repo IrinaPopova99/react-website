8b0c5db62f9727ea563aa97149b8dcd971cfcd2d
IrinaPopova99 <janneteldenorr@gmail.com>
2021-03-18 17:07:10 +0700

    Create website with React

---
+++ react-website/src/components/Cards.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import CardItem from './CardItem';
+import './Cards.css';
+
+function Cards() {
+    return (
+        <div className="cards">
+            <h1>Check out these EPIC Destinations!</h1>
+            <div className="cards__container">
+                <div className="cards__wrapper">
+                    <ul className="cards__items">
+                        <CardItem  
+                            src="images/img-6.jpg" 
+                            text="Explore this amazing place"
+                            label="Adventure"
+                            path="/services"
+                        />
+                        <CardItem  
+                            src="images/img-5.jpg" 
+                            text="Travel through this Chain of Mountains"
+                            label="Extreme"
+                            path="/services"
+                        />
+                    </ul>
+                    <ul className="cards__items">
+                        <CardItem  
+                            src="images/img-1.jpg" 
+                            text="Explore this amazing place"
+                            label="Adventure"
+                            path="/services"
+                        />
+                        <CardItem  
+                            src="images/img-2.jpg" 
+                            text="Travel through this Chain of Mountains"
+                            label="Extreme"
+                            path="/services"
+                        />
+                        <CardItem  
+                            src="images/img-3.jpg" 
+                            text="Allow yourself relax on this exotic beach"
+                            label="Beach"
+                            path="/services"
+                        />
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cards;
